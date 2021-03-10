b8c9fb877eff311a7800db3481edc0fccee400e3
kristiyansakalyan <57534207+kristiyansakalyan@users.noreply.github.com>
2021-03-10 20:51:44 +0100

    Fetching, Filtering and Formating; Symbols mapping

--- src/analytics/backtesting/backtesting.js
+++ src/analytics/backtesting/backtesting.js
@@ -1,17 +1,48 @@
 // Just an example of how this file could look like
 
+/*
+stocksData = 
+{
+    "IBM": {
+        "2021-03-08": {
+            "1. open": "122.9900",
+            "2. high": "126.8500",
+            "3. low": "122.8800",
+            "4. close": "124.8100",
+            "5. volume": "7239191"
+        }
+    }, 
+    "TSLA": {
+
+    }
+}
+
+{
+    "Tesla": "TSLA",
+}
+
+
+*/
+
 // Step 1: Final Portfolio Balance
-function finalPortfolioBalance(portfolio, stocks) {}
+function finalPortfolioBalance(portfolio, stocksData) {}
 
 // Step 2: Max Drawdown
-function maxDrawdown(portfolio, stocks) {}
+function maxDrawdown(portfolio, stocksData) {}
 
 // Step 3: Standard Deviation and Sharpe Ratio
-function standardDeviation(portfolio, stocks) {}
+function standardDeviation(portfolio, stocksData) {}
 
-function sharpeRatio(portfolio, stocks) {}
+function sharpeRatio(portfolio, stocksData) {}
 
 // Step 5: Performance in Best and Worst Year
-function performanceBestYear(portfolio, stocks) {}
+function performanceBestYear(portfolio, stocksData) {}
 
-function performanceWorstYear(portfolio, stocks) {}
+function performanceWorstYear(portfolio, stocksData) {}
+
+exports.finalPortfolioBalance = finalPortfolioBalance;
+exports.maxDrawdown = maxDrawdown;
+exports.standardDeviation = standardDeviation;
+exports.sharpeRatio = sharpeRatio;
+exports.performanceBestYear = performanceBestYear;
+exports.performanceWorstYear = performanceWorstYear;
