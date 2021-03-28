96756b1879cb57720db9b746b42082cdc4bfa539
ge49xox <ge49xox@mytum.de>
2021-03-28 15:37:48 +0300

    Updated PEration and Dividendyield

--- src/analytics/javascript-analysis/Dividendyield.js
+++ src/analytics/javascript-analysis/Dividendyield.js
@@ -1,17 +1,29 @@
 /**
- * Returns the dividendyield in % of the company overview
+ * Returns the average, total and the dividendyield in % of the company overview
  * @param {{symbol1: {}, symbolN: {}}} symbolCompanyOverview 
  * @returns  {{
  * dividendyield: {dividendyield: dividendyield%}
+ * averageDividendyield: {averageDividendyield: averageDividendyield}
+ * totalDividendyield: {totalDividendyield: totalDividendyield%}
  * }}
  */
 function getDividendyield(symbolCompanyOverview) {
     let dividendyield = {};
+    let totalDividendyield = 0;
+    let averageDividendyield = 0;
     Object.keys(symbolCompanyOverview).forEach((symbol) => {
-        let content = ymbolCompanyOverview[symbol].DividendYield;
+        let content = symbolCompanyOverview[symbol].DividendYield;
+        totalDividendyield += content;
         dividendyield[content] = (parseFloat(content) * 100).toFixed(2) + "%";
     });
+    averageDividendyield = totalDividendyield / symbolCompanyOverview.length;
+    totalDividendyield = (parseFloat(totalDividendyield) * 100).toFixed(2) + "%";
+    averageDividendyield = (parseFloat(averageDividendyield) * 100).toFixed(2) + "%";
     return {
-        dividendyield
+        dividendyield,
+        averageDividendyield,
+        totalDividendyield
     };
 }
+
+exports.getDividendyield = getDividendyield;
--- src/analytics/javascript-analysis/PriceEarningRation.js
+++ src/analytics/javascript-analysis/PriceEarningRation.js
@@ -1,17 +1,25 @@
 /**
- * Returns the price-earning ratio from the company overview
+ * Returns the price-earning, average and total ratio from the company overview
  * @param {{symbol1: {}, symbolN: {}}} symbolCompanyOverview 
  * @returns  {{
- * peRatio: {peRatio: peRatio}
+ * peRatio: {peRatio: peRatio},
+ * averagePEration: {averagePEration: averagePEration}
+ * totalPEratio: {totalPEratio: totalPEratio}
  * }}
  */
 function getPriceEarningRation(symbolCompanyOverview) {
     let peRation = {};
+    let totalPEratio = 0;
+    let averagePEration = 0;
     Object.keys(symbolCompanyOverview).forEach((symbol) => {
         peRation[symbolCompanyOverview[symbol].PERatio] = symbolCompanyOverview[symbol].PERatio;
+        totalPEratio += symbolCompanyOverview[symbol].PERatio;
     });
+    averagePEration = totalPEratio / symbolCompanyOverview.length;
     return {
-        peRation
+        peRation,
+        averagePEration,
+        totalPEratio
     };
 }
 
